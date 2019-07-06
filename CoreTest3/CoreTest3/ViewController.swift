bd2bddfc3a0e7333dfbc86b18aaee6f0488f9607
Viktor Varsano <Viktor_varsano@hotmail.com>
2019-07-06 12:20:43 +0300

    Persisting data using Core Data

---
+++ CoreTest3/CoreTest3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreTest3
+//
+//  Created by Viktor Varsano on 2.07.19.
+//  Copyright © 2019 Viktor Varsano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
